c58d354f5525720ca500c0388a5bcd2c88d93b28
michelle-mok <michellemokhuihui@gmail.com>
2021-07-06 11:30:32 +0800

    changed username in config.js file

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
 module.exports = {
   development: {
-    username: 'michellemok',
+    username: '<YOUR_USERNAME>',
     password: null,
     database: 'bugs_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
 };
-
